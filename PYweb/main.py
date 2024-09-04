842d4c51b7ddbf81724576b1c619d8822dc4a627
lnk137 <1197933911@qq.com>
2024-09-04 23:04:49 +0800

    获取黑色区域面积等，逻辑调整

--- PYweb/main.py
+++ PYweb/main.py
@@ -43,6 +43,16 @@ def get_data():
         "soil_width": soil_width,
         "start_height": start_height,
     })
+    
+def calculate_black_area_ratio(img_pil):
+    img = np.array(img_pil.convert("L"))  # 转换为灰度图像数组
+    total_pixels = img.size
+    black_pixels = np.sum(img <= 150)  # 计算黑色像素数量
+    black_ratio = black_pixels / total_pixels  # 计算黑色像素比例
+    print(f"黑色像素比例{black_ratio},{total_pixels}个像素,{black_pixels}个黑色像素")
+    height, width = img.shape
+    black_area = black_ratio * height * width * 0.01  # 计算黑色区域面积
+    return black_area, black_ratio
 
 def parse_resolution(resolution_str):
     # 解析分辨率字符串并返回宽度和高度
@@ -87,15 +97,7 @@ def is_grayscale_image(img_pil):
         return True
     
     return False
-
-
-def process_image(img_pil, lower_range, upper_range, resolution):
-    # 如果图像是灰度图，则跳过颜色处理
-    if is_grayscale_image(img_pil):
-        print("灰度图检测到，跳过颜色处理")
-        img_resized = resize_image(img_pil, resolution)  # 调整分辨率
-        return img_resized
-
+def black_white_processing(img_pil,resolution):
     # 处理图像，先调整分辨率再进行颜色操作
     img_resized = resize_image(img_pil, resolution)  # 调整分辨率
 
@@ -103,18 +105,30 @@ def process_image(img_pil, lower_range, upper_range, resolution):
     img_cv = cv2.cvtColor(img_cv, cv2.COLOR_RGB2BGR)  # 将RGB格式转换为BGR格式
     hsv_img = cv2.cvtColor(img_cv, cv2.COLOR_BGR2HSV)  # 将图像转换为HSV色彩空间
 
-    if len(lower_range) == 3 and len(upper_range) == 3:
-        mask = cv2.inRange(hsv_img, np.array(lower_range), np.array(upper_range))  # 创建颜色掩码
-
-        # 反转掩码的黑白颜色
-        inverted_mask = cv2.bitwise_not(mask)
-
-        # 将处理后的掩码转换为三通道的图像以便返回和显示
-        mask_rgb = cv2.cvtColor(inverted_mask , cv2.COLOR_GRAY2RGB)
-
-        return Image.fromarray(mask_rgb)  # 将掩码图像转换为PIL图像并返回
-    else:
+    if len(lower_range_hsv) != 3 and len(upper_range_hsv) != 3:
         raise ValueError("lower_range和upper_range必须是长度为3的列表或数组")
+    # 创建颜色掩码
+    mask = cv2.inRange(hsv_img, np.array(lower_range_hsv), np.array(upper_range_hsv)) 
+    # 反转掩码的黑白颜色
+    inverted_mask = cv2.bitwise_not(mask)
+    # 将处理后的掩码转换为三通道的图像以便返回和显示
+    mask_rgb = cv2.cvtColor(inverted_mask , cv2.COLOR_GRAY2RGB)
+    # 将掩码图像转换为PIL图像
+    img_pil=Image.fromarray(mask_rgb)  
+    return img_pil
+
+def process_image(img_pil, lower_range, upper_range, resolution):
+    global final_img, S_Black, y_coordinate, black_ratio
+    # 如果图像不是灰度图，则进入颜色处理
+    if not is_grayscale_image(img_pil):
+        print("是原图，进入颜色处理")
+        img_pil=black_white_processing(img_pil,resolution)
+    else:
+        img_pil = resize_image(img_pil, resolution)  # 调整分辨率
+        print("是黑白图，取消颜色处理")
+    S_Black, black_ratio = calculate_black_area_ratio(img_pil)
+    print(f"黑色区域面积: {S_Black:.2f}, 黑色像素比例: {black_ratio:.2f}")
+    return img_pil
 
 @app.route("/upload", methods=["POST"])
 def upload_image():
